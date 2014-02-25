57deef02c1ff48ef0c55d34506217b5df20bdd54
Gary Burgess <gary.burgess@gmail.com>
2014-02-25 21:25:52 +0000

    MaybeT

---
+++ src/Control.Monad.Maybe.Trans.purs
@@ -0,0 +1,23 @@
+module Control.Monad.Maybe.Trans where
+
+import Prelude
+import Data.Maybe
+import Control.Monad
+import Control.Monad.Trans
+
+data MaybeT m a = MaybeT (m (Maybe a))
+
+instance (Prelude.Monad m) => Prelude.Monad (MaybeT m) where
+  --return = lift <<< return -- TODO: doesn't type check?
+  return x = MaybeT $ return $ Just x
+  (>>=) x f = MaybeT $ do
+    v <- runMaybeT x
+    case v of
+      Nothing -> return Nothing
+      Just y  -> runMaybeT (f y)
+
+instance Control.Monad.Trans.MonadTrans MaybeT where
+  lift = MaybeT <<< liftM Just
+
+runMaybeT :: forall m a. MaybeT m a -> m (Maybe a)
+runMaybeT (MaybeT x) = x
