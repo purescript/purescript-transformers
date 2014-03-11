5ee63adc067196a31d510eea7efd6eb2a15bcc08
Gary Burgess <gary.burgess@gmail.com>
2014-03-11 21:03:14 +0000

    Add additional state running functions

--- src/Control/Monad/State/Trans.purs
+++ src/Control/Monad/State/Trans.purs
@@ -12,7 +12,7 @@ instance monadStateT :: (Monad m) => Monad (StateT s m) where
   (>>=) (StateT x) f = StateT \s -> do
     { state = s', value = v } <- x s
     runStateT (f v) s'
-    
+
 instance monadTransStateT :: MonadTrans (StateT s) where
   lift m = StateT \s -> do
     x <- m
@@ -20,6 +20,15 @@ instance monadTransStateT :: MonadTrans (StateT s) where
 
 runStateT :: forall s m a. StateT s m a -> s -> m (StateData s a)
 runStateT (StateT s) = s
+
+evalStateT :: forall s m a. (Monad m) => StateT s m a -> s -> m a
+evalStateT m s = runStateT m s >>= \x -> return x.value
+
+execStateT :: forall s m a. (Monad m) => StateT s m a -> s -> m s
+execStateT m s = runStateT m s >>= \x -> return x.state
+
+mapStateT :: forall s m1 m2 a b. (m1 (StateData s a) -> m2 (StateData s b)) -> StateT s m1 a -> StateT s m2 b
+mapStateT f m = StateT $ f <<< runStateT m
 
 withStateT :: forall s m a. (s -> s) -> StateT s m a -> StateT s m a
 withStateT f s = StateT $ runStateT s <<< f
