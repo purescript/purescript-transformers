2e3ace77db0dee9c9c4e372ea347f5b0a2640f0b
Gary Burgess <gary.burgess@gmail.com>
2014-03-11 20:55:54 +0000

    Remove MonadEff from MonadTrans

--- src/Control/Monad/Trans.purs
+++ src/Control/Monad/Trans.purs
@@ -1,10 +1,6 @@
 module Control.Monad.Trans where
 
 import Prelude
-import Control.Monad.Eff
 
 class MonadTrans t where
   lift :: forall m a. (Monad m) => m a -> t m a
-  
-class MonadEff m where
-  liftEff :: forall eff a. Eff eff a -> m a
