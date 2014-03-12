9c33410c9c001e5ad4d4f77d0d8ff8a9fcdbb887
Gary Burgess <gary.burgess@gmail.com>
2014-03-12 21:15:04 +0000

    Add missing mapMaybeT

--- src/Control/Monad/Maybe/Trans.purs
+++ src/Control/Monad/Maybe/Trans.purs
@@ -8,7 +8,6 @@ import Control.Monad.Trans
 data MaybeT m a = MaybeT (m (Maybe a))
 
 instance monadMaybeT :: (Monad m) => Monad (MaybeT m) where
-  --return = lift <<< return -- TODO: doesn't type check?
   return x = MaybeT $ return $ Just x
   (>>=) x f = MaybeT $ do
     v <- runMaybeT x
@@ -21,6 +20,9 @@ instance monadTransMaybeT :: MonadTrans MaybeT where
 
 runMaybeT :: forall m a. MaybeT m a -> m (Maybe a)
 runMaybeT (MaybeT x) = x
+
+mapMaybeT :: forall m1 m2 a b. (m1 (Maybe a) -> m2 (Maybe b)) -> MaybeT m1 a -> MaybeT m2 b
+mapMaybeT f = MaybeT <<< f <<< runMaybeT
 
 liftCatchMaybe :: forall m e a. (m (Maybe a) -> (e -> m (Maybe a)) -> m (Maybe a)) -> MaybeT m a -> (e -> MaybeT m a) -> MaybeT m a
 liftCatchMaybe catch m h = MaybeT $ catch (runMaybeT m) (runMaybeT <<< h)
