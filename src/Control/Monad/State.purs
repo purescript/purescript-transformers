bd5c2b6bc05ce2c7683d4ffcb2e6b053f5e80876
Gary Burgess <gary.burgess@gmail.com>
2014-03-16 11:50:51 +0000

    Add missing evalState, execState, mapState

--- src/Control/Monad/State.purs
+++ src/Control/Monad/State.purs
@@ -10,5 +10,14 @@ type State s a = StateT s Identity a
 runState :: forall s a. State s a -> s -> Tuple a s
 runState s = runIdentity <<< runStateT s
 
+evalState :: forall s a. State s a -> s -> a
+evalState m s = fst (runState m s)
+
+execState :: forall s a. State s a -> s -> s
+execState m s = snd (runState m s)
+
+mapState :: forall s a b. (Tuple a s -> Tuple b s) -> State s a -> State s b
+mapState f = mapStateT (Identity <<< f <<< runIdentity)
+
 withState :: forall s a. (s -> s) -> State s a -> State s a
 withState = withStateT
