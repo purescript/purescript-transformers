a24afa9eb718aed3b230ac1ff382feb55e2dff58
Gary Burgess <gary.burgess@gmail.com>
2014-03-16 11:50:41 +0000

    Add missing mapWriter

--- src/Control/Monad/Writer.purs
+++ src/Control/Monad/Writer.purs
@@ -10,3 +10,6 @@ type Writer w a = WriterT w Identity a
 
 runWriter :: forall w a. Writer w a -> Tuple a w
 runWriter = runIdentity <<< runWriterT
+
+mapWriter :: forall w1 w2 a b. (Tuple a w1 -> Tuple b w2) -> Writer w1 a -> Writer w2 b
+mapWriter f = mapWriterT (Identity <<< f <<< runIdentity)
