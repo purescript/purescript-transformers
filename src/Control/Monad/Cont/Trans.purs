b038e6ea62174d0f9b159cdf3f85e793dd627f65
eric thul <thul.eric@gmail.com>
2014-04-06 11:31:22 -0400

    Initial implementation of ContT
    Based off of:
    https://gist.github.com/paf31/2dfcefe275db262beb42

---
+++ src/Control/Monad/Cont/Trans.purs
@@ -0,0 +1,32 @@
+module Control.Monad.Cont.Trans where
+
+import Prelude
+import Control.Monad.Trans
+
+data ContT r m a = ContT ((a -> m r) -> m r)
+
+runContT :: forall r m a. ContT r m a -> (a -> m r) -> m r
+runContT (ContT f) k = f k
+
+mapContT :: forall r m a. (m r -> m r) -> ContT r m a -> ContT r m a
+mapContT f m = ContT (\k -> f $ runContT m k)
+
+withContT :: forall r m a b. ((b -> m r) -> (a -> m r)) -> ContT r m a -> ContT r m b
+withContT f m = ContT (\k -> (runContT m) (f k))
+
+callCC :: forall r m a b. ((a -> ContT r m b) -> ContT r m a) -> ContT r m a
+callCC f = ContT (\k -> runContT (f (\a -> ContT (\_ -> k a))) k)
+
+instance functorContT :: (Monad m) => Functor (ContT r m) where
+  (<$>) f m = ContT (\k -> runContT m (\a -> k $ f a))
+
+instance applicativeContT :: (Functor m, Monad m) => Applicative (ContT r m) where
+  pure a = ContT (\k -> k a)
+  (<*>) f v = ContT (\k -> runContT f $ (\g -> runContT v (\a -> (k $ g a))))
+
+instance monadContT :: (Monad m) => Monad (ContT r m) where
+  return a = ContT (\k -> k a)
+  (>>=) m k = ContT (\k' -> runContT m (\a -> runContT (k a) k'))
+
+instance monadTransContT :: MonadTrans (ContT r) where
+  lift m = ContT (\k -> m >>= k)
