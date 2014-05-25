3eff2993dccfdc771f7560f45f26cd9f0ed08f79
Gary Burgess <gary.burgess@gmail.com>
2014-05-25 12:44:44 +0100

    Update build

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,73 @@
 module.exports = function(grunt) {
 
-    "use strict";
+  "use strict";
 
-    grunt.initConfig({ 
+  grunt.initConfig({ 
+  
+    libFiles: [
+      "src/**/*.purs",
+      "bower_components/purescript-*/src/**/*.purs*"
+    ],
+  
+    clean: ["output", "tmp"],
     
-        clean: ["externs", "js"],
-        
-        "purescript-make": {
-            options: {
-                tco: true,
-                magicDo: true
-            },
-            lib: {
-                options: { make: true },
-                files: { _: ["src/**/*.purs", "bower_components/purescript-*/src/**/*.purs"] }
-            }
-        },
-    
-        purescript: {
-            options: {
-                main: true
-            },
-            exampleReader: {
-                files: {
-                    "js/_examples/Reader.js": ["examples/Reader.purs", "src/**/*.purs", "bower_components/purescript-*/src/**/*.purs"]
-                }
-            },
-            exampleState: {
-                files: {
-                    "js/_examples/State.js": ["examples/State.purs", "src/**/*.purs", "bower_components/purescript-*/src/**/*.purs"]
-                }
-            },
-            exampleWriter: {
-                files: {
-                    "js/_examples/Writer.js": ["examples/Writer.purs", "src/**/*.purs", "bower_components/purescript-*/src/**/*.purs"]
-                }
-            },
-            exampleCont: {
-                files: {
-                    "js/_examples/Cont.js": ["examples/Cont.purs", "src/**/*.purs", "bower_components/purescript-*/src/**/*.purs"]
-                }
-            }
+    pscMake: ["<%=libFiles%>"],
+    dotPsci: ["<%=libFiles%>"],
+    docgen: {
+        readme: {
+            src: "src/**/*.purs",
+            dest: "README.md"
         }
-        
-    });
+    },
+  
+    psc: {
+      options: {
+        main: true
+      },
+      exampleReader: {
+        src: ["examples/Reader.purs", "<%=libFiles%>"],
+        dest: "tmp/Reader.js"
+      },
+      exampleState: {
+        src: ["examples/State.purs", "<%=libFiles%>"],
+        dest: "tmp/State.js"
+      },
+      exampleWriter: {
+        src: ["examples/Writer.purs", "<%=libFiles%>"],
+        dest: "tmp/Writer.js"
+      },
+      exampleCont: {
+        src: ["examples/Cont.purs", "<%=libFiles%>"],
+        dest: "tmp/Cont.js"
+      }
+    },
+    
+    execute: {
+      exampleReader: {
+        src: "tmp/Reader.js"
+      },
+      exampleState: {
+        src: "tmp/State.js"
+      },
+      exampleWriter: {
+        src: "tmp/Writer.js"
+      },
+      exampleCont: {
+        src: "tmp/Cont.js"
+      }
+    }
+    
+  });
 
-    grunt.loadNpmTasks("grunt-purescript");
-    grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-purescript");
+  grunt.loadNpmTasks("grunt-execute");
+  grunt.loadNpmTasks("grunt-contrib-clean");
 
-    grunt.registerTask("default", ["purescript-make:lib"]);
+  grunt.registerTask("exampleReader", ["psc:exampleReader", "execute:exampleReader"]);
+  grunt.registerTask("exampleState", ["psc:exampleState", "execute:exampleState"]);
+  grunt.registerTask("exampleWriter", ["psc:exampleWriter", "execute:exampleWriter"]);
+  grunt.registerTask("exampleCont", ["psc:exampleCont", "execute:exampleCont"]);
+  grunt.registerTask("examples", ["psc", "execute"]);
+  grunt.registerTask("make", ["pscMake", "dotPsci", "docgen"]);
+  grunt.registerTask("default", ["make"]);
 };
