cb1466a0324b9869841fc9bbd767069b990709a8
Phil Freeman <paf31@cantab.net>
2014-08-26 21:02:24 -0700

    Remove unused imports

--- src/Control/Monad/Error.purs
+++ src/Control/Monad/Error.purs
@@ -1,10 +1,5 @@
 module Control.Monad.Error where
 
-import Control.Alt
-import Control.Alternative
-import Control.Plus
-import Data.Either
-
 class Error a where
   noMsg :: a
   strMsg :: String -> a
