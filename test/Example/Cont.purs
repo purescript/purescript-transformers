847f03e8c00dd329e040a23b14bd5ecf2ea6a3d4
Gary Burgess <gary.burgess@gmail.com>
2017-03-12 23:34:28 +0000

    Update for PureScript 0.11

--- test/Example/Cont.purs
+++ test/Example/Cont.purs
@@ -7,10 +7,10 @@ import Control.Monad.Eff.Class (class MonadEff, liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Cont.Trans (ContT, runContT, callCC)
 
-main0 :: forall s m eff. (MonadEff (console :: CONSOLE | eff) m) => ContT s m String
+main0 :: forall s m eff. MonadEff (console :: CONSOLE | eff) m => ContT s m String
 main0 = callCC \k -> do
-  liftEff $ log "Before the continuation"
-  k "You should see this."
+  void $ liftEff $ log "Before the continuation"
+  void $ k "You should see this."
   k "You should not see this."
 
 main :: forall eff. Eff (console :: CONSOLE | eff) Unit
