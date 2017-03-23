4e818a6ca5e1f100af798338dc4c3e450defc4d9
Matthew Leon <ml@matthewleon.com>
2017-03-23 15:58:09 +0000

    Adapt runListT from Haskell's List.Transformer

--- src/Control/Monad/List/Trans.purs
+++ src/Control/Monad/List/Trans.purs
@@ -20,6 +20,7 @@ module Control.Monad.List.Trans
   , prepend
   , prepend'
   , repeat
+  , runListT
   , scanl
   , singleton
   , tail
@@ -70,9 +71,9 @@ data Step a s
   | Skip (Lazy s)
   | Done
 
--- | Run a computation in the `ListT` monad.
-runListT :: forall f a. ListT f a -> f (Step a (ListT f a))
-runListT (ListT fa) = fa
+-- | Drain a `ListT`, running it to completion and discarding all values.
+runListT :: forall f a. Monad f => ListT f a -> f Unit
+runListT = foldl' (\_ action -> void (pure action)) unit
 
 -- | The empty list.
 nil :: forall f a. Applicative f => ListT f a
